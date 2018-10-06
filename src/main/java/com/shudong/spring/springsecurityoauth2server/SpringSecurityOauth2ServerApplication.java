29516c726f99d2b47b7daa02b2d028564f724db8
shudong <zhangshudong@gmail.com>
2018-10-06 22:01:49 +0800

    Initial commit

---
+++ src/main/java/com/shudong/spring/springsecurityoauth2server/SpringSecurityOauth2ServerApplication.java
@@ -0,0 +1,12 @@
+package com.shudong.spring.springsecurityoauth2server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauth2ServerApplication.class, args);
+	}
+}
